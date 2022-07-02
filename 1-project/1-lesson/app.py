5092cf42d9c7ddfe02e8622d4ebe618de5f48085
billwang7599 <billwang7599@gmail.com>
2022-07-02 16:12:26 -0400

    edited lesson-1 code | removed setNum

--- 1-project/1-lesson/app.py
+++ 1-project/1-lesson/app.py
@@ -2,26 +2,19 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import *
 import sys
 
+
 def window():
-    app = QApplication(sys.argv)
     win = QMainWindow()
 
-            # window
+        # window
     # set the title
     win.setWindowTitle("Project 1")
 
     # set window pos and win size (x, y, width, height)
     win.setGeometry(100, 100, 500, 500)
-    # set fixed width and height
-    win.setFixedWidth(500)
-    win.setFixedHeight(500)
 
             # label
-    # prints "Hello world" onto app
-    label = QtWidgets.QLabel("test start", win)
-    #replaces text with new text 
-    label.setText("Start the count!")
-    label.setNum(0)
+    label = QtWidgets.QLabel("0", win)
     label.adjustSize()
     label.move(int(win.width()/2 - label.width()/2), label.height()) # moves label to middle of screen
 
@@ -33,13 +26,14 @@ def window():
     
 
     win.show()
-    sys.exit(app.exec_())
 
 def button_output(win, text):
-    text.setNum(int(text.text()) + 1)
+    text.setText(str(int(text.text()) + 1))
 
     # adjust label size so text fits
     text.adjustSize()
     text.move(win.width()/2 - text.width()/2, text.height())
 
+app = QApplication(sys.argv)
 window()
+sys.exit(app.exec_())
