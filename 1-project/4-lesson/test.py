47c9b0ef3b2e1811ce07831c818265b35e08ddbf
billwang7599 <billwang7599@gmail.com>
2022-07-02 16:14:27 -0400

    edited lesson-1 code | removed setNum

--- 1-project/4-lesson/test.py
+++ 1-project/4-lesson/test.py
@@ -2,46 +2,20 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import *
 import sys
 
-def window():
-    app = QApplication(sys.argv)
-    win = QWidget()
+app = QApplication(sys.argv)
+win = QWidget() # window
 
-            # window
-    # set the title
-    win.setWindowTitle("Lesson 1")
+# set window pos and win size (x, y, width, height)
+win.setGeometry(100, 100, 500, 500)
 
-    # set window pos and win size (x, y, width, height)
-    win.setGeometry(100, 100, 500, 500)
-    # set fixed width and height
-    '''
-    win.setFixedWidth(500)
-    win.setFixedHeight(500)
-    '''
+button = QPushButton('I am a button', win)
+label = QLabel('I am a label', win)
 
-            # label
-    # prints "Hello world" onto app
-    label = QtWidgets.QLabel("test start", win)
-    #replaces text with new text 
-    label.setText("Start the count!")
-    label.setNum(0)
-    label.adjustSize()
-    label.move(int(win.width()/2 - label.width()/2), label.height()) # moves label to middle of screen
+button.move(50, 50)
+button.setText('hello')
+button.resize(100, 100)
+print(button.x())
 
-            # button 
-    button = QPushButton("Click me", win)
-    button.move(int(win.width()/2 - button.width()/2) , 300)
-    # Connect the button to the function
-    button.clicked.connect(lambda : button_output(win, label))
-    
+win.show()
+sys.exit(app.exec_())
 
-    win.show()
-    sys.exit(app.exec_())
-
-def button_output(win, text):
-    text.setNum(int(text.text()) + 1)
-
-    # adjust label size so text fits
-    text.adjustSize()
-    text.move(win.width()/2 - text.width()/2, text.height())
-
-window()
